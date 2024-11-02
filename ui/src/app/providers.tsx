77dda19cc09b821cd014fc12532427299fc87d3e
ivanepopov <popoveivan@gmail.com>
2024-11-02 18:46:41 -0500

    moved NextUIProvider to providers

--- ui/src/app/providers.tsx
+++ ui/src/app/providers.tsx
@@ -1,13 +1,16 @@
 'use client'
 import { ThemeProvider } from "next-themes"
-import React from "react"
 import { SearchProvider } from "./providers/SearchProvider"
+import { NextUIProvider } from "@nextui-org/react";
+import { ReactNode } from "react";
 
-export function Providers({ children } : { children : React.ReactNode}) {
+export function Providers({ children } : { children : ReactNode}) {
     return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <SearchProvider>
-        {children}
+        <NextUIProvider>
+          {children}
+        </NextUIProvider>
       </SearchProvider>
     </ThemeProvider>
     )
