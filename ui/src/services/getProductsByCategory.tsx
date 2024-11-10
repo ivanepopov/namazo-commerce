20ee896090ff03607a851b9357a1d1b7e64b72aa
ivanepopov <popoveivan@gmail.com>
2024-11-10 15:52:33 -0600

    parameter for page number

--- ui/src/services/getProductsByCategory.tsx
+++ ui/src/services/getProductsByCategory.tsx
@@ -2,14 +2,15 @@ import { Product } from "@/types/Product"
 import axios from "axios"
 /* * 
  * @param: category - Category to get products from
+ * @param: page - Which batch of products to return
  * @param: url - Optional url used for testing purposes
  * 
  * @return: Product[]
  * *******************************************************************/
-async function getProductsByCategory (category : string, url? : string) {
+async function getProductsByCategory (category : string, page : number, url? : string) {
 
     let data: Product[] = []
-    await axios.get(`/api/${category}`)
+    await axios.get(`/api/${category}`, { params: { page } })
         .then((res) => data = res.data)
         .catch(e => console.log(e.message))
     
