bfe8a19fbb5d3a2572a77fd0af4590659374da54
ivanepopov <popoveivan@gmail.com>
2024-11-18 18:07:52 -0600

    HomeNamazoInfo component init

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,10 +1,12 @@
 import HomeCategoryList from "@/components/home/HomeCategoryList";
+import HomeNamazoInfo from "@/components/home/HomeNamazoInfo";
 
 export default function Home() {
 
   return (
     <div className="h-page overflow-scroll snap-y snap-mandatory scrollbar-hide">
       <HomeCategoryList />
+      <HomeNamazoInfo />
     </div>
   );
 }
---
+++ ui/src/components/home/HomeNamazoInfo.tsx
@@ -0,0 +1,15 @@
+/* * 
+ * Home Page Component that displays namazo information
+ * *******************************************************************/
+function HomeNamazoInfo() {
+  return (
+    <section className="snap-center">
+      <div className="w-screen h-page flex items-center justify-center">
+        <h1 className="text-black dark:text-white text-2xl">Namazo Information</h1>
+      </div>
+    </section>
+  )
+}
+
+
+export default HomeNamazoInfo
