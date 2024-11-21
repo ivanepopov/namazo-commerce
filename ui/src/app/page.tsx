f76d70e350747bc39a0704577ad3904b5358375b
ivanepopov <popoveivan@gmail.com>
2024-11-21 12:11:11 -0600

    getCategories in page component

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,11 +1,36 @@
+'use client'
 import HomeCategoryList from "@/components/home/HomeCategoryList";
 import HomeNamazoInfo from "@/components/home/HomeNamazoInfo";
+import LoadingBar from "@/components/LoadingBar";
+import { useSearchContext } from "@/providers/SearchProvider";
+import getCategories from "@/services/getCategories";
+import { Category } from "@/types/Category";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
+  const { router, refreshData, setRefreshData } = useSearchContext()
+  const [categories, setCategories] = useState<Category[]>()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function fetchCategories() {
+      const fetchedCategories: Category[] = await getCategories()
+
+      setCategories(fetchedCategories)
+      setLoading(false)
+    }
+
+    fetchCategories()
+    setRefreshData(!refreshData)
+  }, [])
+
+  if (loading || categories === undefined)
+    return <LoadingBar />
+    
   return (
     <div className="h-page overflow-scroll snap-y snap-mandatory scrollbar-hide">
-      <HomeCategoryList />
+      <HomeCategoryList categories={categories} router={router}/>
       <HomeNamazoInfo />
     </div>
   );
--- ui/src/components/home/HomeCategoryList.tsx
+++ ui/src/components/home/HomeCategoryList.tsx
@@ -1,39 +1,26 @@
-'use client'
-import { useEffect, useState } from "react"
-import { useSearchContext } from "@/providers/SearchProvider"
-import getCategories from "@/services/getCategories"
-import LoadingBar from "@/components/LoadingBar"
 import CategoryCard from "@/components/category/CategoryCard"
 import { Category } from "@/types/Category"
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime"
+import { IoIosArrowDown } from "react-icons/io"
 
+type Props = {
+  categories: Category[]
+  router: AppRouterInstance
+}
 /* * 
  * Home Page Component that displays a list of categories
  * *******************************************************************/
-function HomeCategoryList() {
+function HomeCategoryList(props: Props) {
 
-  const { router, refreshData, setRefreshData } = useSearchContext()
-  const [categories, setCategories] = useState<Category[]>()
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    async function fetchCategories() {
-      const fetchedCategories: Category[] = await getCategories()
-
-      setCategories(fetchedCategories)
-      setLoading(false)
-    }
-
-    fetchCategories()
-    setRefreshData(!refreshData)
-  }, [])
-
-  if (loading || categories === undefined)
-    return <LoadingBar />
+  const { categories, router } = props
 
   return (
-    <section className="snap-center">
-      <div className="w-screen h-page flex p-4 space-x-4 justify-center">
+    <section className="flex flex-col w-screen h-page justify-between snap-center">
+      <div className="flex p-4 space-x-4 justify-center">
       { categories.map(c => <CategoryCard key={c.name} category={c.name} image={c.image} router={router} />) }
+      </div>
+      <div className="flex justify-center items-center h-16 bg-gradient-to-b from-[#282828] to-[#484848]">
+        <IoIosArrowDown className="text-black dark:text-white " size={32} />
       </div>
     </section>
   )
