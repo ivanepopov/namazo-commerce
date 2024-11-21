f0ee1e752263734ba01a56cbfb67ca114653ea05
ivanepopov <popoveivan@gmail.com>
2024-11-21 12:11:49 -0600

    Added type prop to return icon

--- ui/src/components/NamazoImage.tsx
+++ ui/src/components/NamazoImage.tsx
@@ -3,15 +3,27 @@ import Link from "next/link"
 import Image from "next/image"
 import namazo_light from "@/util/img/namazo_light.png"
 import namazo_dark from "@/util/img/namazo_dark.png"
+import namazo_icon from "@/util/img/namazo_icon.png"
 import { useSearchContext } from "@/providers/SearchProvider"
+
+type Props = {
+  link: boolean
+  type?: string
+}
 
 /* *
  * Namazo Image component, returns dark image on light mode, and
  * light image on dark mode for clarity
  * *******************************************************************/
-function NamazoImage({ link } : { link : boolean }) {
+function NamazoImage(props: Props) {
 
+  const { link, type } = props
   const { setSearchTerm } = useSearchContext()
+
+  if (type == "icon")
+    return (
+      <Image title="namazo_icon" priority={true} className="rounded-full shadow-2xl" height="256" width="256" alt="icon" src={namazo_icon}/>
+    )
 
   return link ?
       <div>
